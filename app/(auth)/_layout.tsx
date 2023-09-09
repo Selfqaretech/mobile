a1f8fd3f67179dd4b87424a82cdf9fee4a91a1f2
Samuel Seibidor <samrythmn@gmail.com>
2023-09-09 21:53:52 +0100

    fix: add theme colors

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,35 @@
-import { View, Text } from "react-native";
 import React from "react";
-import { Stack, Slot } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Stack } from "expo-router";
 import useCustomTheme from "@src/hooks/useCustomTheme";
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Location } from "iconsax-react-native";
+import CustomText from "@src/component/text";
 
 const Layout = () => {
-  const {} = useCustomTheme();
+  const { theme } = useCustomTheme();
+  const { top } = useSafeAreaInsets();
   return (
-    <>
-      <Stack.Screen
-        options={{ headerShown: false, statusBarTranslucent: true }}
-      />
-      <Slot />
-    </>
+    <Stack
+      screenOptions={{
+        headerShown: true,
+        statusBarTranslucent: false,
+        header: () => (
+          <View
+            style={{
+              padding: 16,
+              paddingTop: 16 + top,
+              backgroundColor: theme.colors.paper,
+              gap: 8,
+              flexDirection: "row",
+            }}
+          >
+            <Location variant="Bold" color={theme.colors.textPrimary} />
+            <CustomText>Location Rd, Port Harcourt</CustomText>
+          </View>
+        ),
+      }}
+    />
   );
 };
 
